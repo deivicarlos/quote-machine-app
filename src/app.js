e7864ef3d2b31e8a13f1199f8e46ab2ec71d1af8
Carlos García <deivicarlos@gmail.com>
2018-03-29 19:34:09 -0400

    first commit

---
+++ src/app.js
@@ -0,0 +1,9 @@
+
+//import './utils.js';
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import 'normalize.css/normalize.css';
+import './styles/styles.scss';
+
+ReactDOM.render(<p>This is my boilerplate</p>, document.getElementById('App'));
